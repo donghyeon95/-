5c55f8e8649c5b12897470de8606c3233eac812a
codongmin <codongmin@gmail.com>
2021-11-04 09:36:49 +0900

    절대반지

--- 10주차/심동민_풀이/절대반지(하).py
+++ 10주차/심동민_풀이/절대반지(하).py
@@ -4,32 +4,14 @@
 '''
 
 
-def solution(rings):
-    i, j = 0, 0
-
-    for _ in range(len(rings)+len(rings)-1):
-        if i >= len(nickname)-1:
-            return 1
-
-        if nickname[i] == rings[j]:
-            i += 1
-        else:
-            i = 0
-
-        j += 1
-        if j == len(rings):
-            j = 0
-    return 0
-
-
 if __name__ == "__main__":
     nickname = input()
     numOfRing = int(input())
-    rings = []
 
-    ans = 0
+    answer = 0
 
-    for i in range(numOfRing):
-        ans += solution(input())
+    for _ in range(numOfRing):
+        if nickname in input()*2:
+            answer += 1
 
-    print(ans)
+    print(answer)
